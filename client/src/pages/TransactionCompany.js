11a66160f4f37bb7f2572637e2b384ebf1f71dac
MichelleLucero <Michelle.Lucero07@myhunter.cuny.edu>
2021-09-06 21:36:42 -0400

    added table

--- client/src/pages/TransactionCompany.js
+++ client/src/pages/TransactionCompany.js
@@ -1,8 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import Search from '../components/search/Search';
 import api from '../utils/api';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
 const TransactionCompany = () => {
+  const classes = useStyles();
+
   // useParams gets it from the link
   const { ticker } = useParams();
   //
@@ -18,40 +36,98 @@ const TransactionCompany = () => {
     getTransactions();
   }, [ticker]);
 
+  // return (
+  //   <div>
+  //     <Search />
+  //     <div>
+  //       <h1>Transactions</h1>
+  //       <p>{ticker}</p>
+  //       <p>{transactions.company.name}</p>
+  //       {transactions.trans &&
+  //         transactions.trans.map((tran) => {
+  //           const {
+  //             trans_uid,
+  //             title,
+  //             date_of_trans,
+  //             transactions_code,
+  //             amount,
+  //             adq_or_disp,
+  //             price,
+  //             shares_owned_after_trans,
+  //             direct_or_indirect_ownership,
+  //             // person_uid,
+  //           } = tran;
+  //           return (
+  //             <div key={trans_uid}>
+  //               <p>{title}</p>
+  //               <p>{date_of_trans}</p>
+  //               <p>{transactions_code}</p>
+  //               <p>{amount}</p>
+  //               <p>{adq_or_disp}</p>
+  //               <p>{price}</p>
+  //               <p>{shares_owned_after_trans}</p>
+  //               <p>{direct_or_indirect_ownership}</p>
+  //             </div>
+  //           );
+  //         })}
+  //     </div>
+  //   </div>
+  // );
   return (
     <div>
-      <div>
-        <h1>Transactions</h1>
-        <p>{ticker}</p>
-        <p>{transactions.company.name}</p>
-        {transactions.trans &&
-          transactions.trans.map((tran) => {
-            const {
-              trans_uid,
-              title,
-              date_of_trans,
-              transactions_code,
-              amount,
-              adq_or_disp,
-              price,
-              shares_owned_after_trans,
-              direct_or_indirect_ownership,
-              // person_uid,
-            } = tran;
-            return (
-              <div key={trans_uid}>
-                <p>{title}</p>
-                <p>{date_of_trans}</p>
-                <p>{transactions_code}</p>
-                <p>{amount}</p>
-                <p>{adq_or_disp}</p>
-                <p>{price}</p>
-                <p>{shares_owned_after_trans}</p>
-                <p>{direct_or_indirect_ownership}</p>
-              </div>
-            );
-          })}
-      </div>
+      <Search />
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label='simple table'>
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell align='right'>Date of Trans</TableCell>
+              <TableCell align='right'>Transactions Code</TableCell>
+              <TableCell align='right'>Amount</TableCell>
+              <TableCell align='right'>Buy or Sale</TableCell>
+              <TableCell align='right'>Price</TableCell>
+              <TableCell align='right'>Shares owned after trans</TableCell>
+              <TableCell align='right'>Direct or indirect ownership</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {transactions.trans &&
+              transactions.trans.map((tran) => {
+                const {
+                  trans_uid,
+                  title,
+                  date_of_trans,
+                  transactions_code,
+                  amount,
+                  adq_or_disp,
+                  price,
+                  shares_owned_after_trans,
+                  direct_or_indirect_ownership,
+                  // person_uid,
+                } = tran;
+
+                return (
+                  <TableRow key={trans_uid}>
+                    <TableCell component='th' scope='row'>
+                      {title}
+                    </TableCell>
+                    <TableCell align='right'>{date_of_trans}</TableCell>
+                    <TableCell align='right'>{transactions_code}</TableCell>
+                    <TableCell align='right'>{amount}</TableCell>
+                    <TableCell align='right'>{adq_or_disp}</TableCell>
+                    <TableCell align='right'>{price}</TableCell>
+                    <TableCell align='right'>
+                      {shares_owned_after_trans}
+                    </TableCell>
+                    <TableCell align='right'>
+                      {direct_or_indirect_ownership}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
