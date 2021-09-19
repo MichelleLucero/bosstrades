8e31c30eab618e677f1f33cd4fea8ad587043cec
MichelleLucero <Michelle.Lucero07@myhunter.cuny.edu>
2021-09-19 18:33:54 -0400

    reorganized search routes

--- server/index.js
+++ server/index.js
@@ -9,103 +9,15 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const app = express();
 const auth = require('./middleware/auth');
-const { reset } = require('nodemon');
+const search = require('./routes/search');
+const port = process.env.port || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Routes
-
-// Return companies and person, ignore later if needed
-app.post('/api/search/', async (req, res) => {
-  try {
-    const { search } = req.body;
-    // Person query
-    const person_result = await db.query(
-      `SELECT * FROM person WHERE name ILIKE '${search}%'`
-    );
-
-    // Company query
-    const company_result = await db.query(
-      `SELECT * FROM company WHERE ticker ILIKE '${search}%'`
-    );
-
-    res
-      .status(200)
-      .json({ persons: person_result.rows, companies: company_result.rows });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
-
-// This creates a link to access person and company based search result
-app.get('/api/search/:search', async (req, res) => {
-  try {
-    const { search } = req.params;
-    // Person query
-    const person_result = await db.query(
-      `SELECT * FROM person WHERE name ILIKE '%${search}%'`
-    );
-
-    // Company query
-    const company_result = await db.query(
-      `SELECT * FROM company WHERE ticker ILIKE '%${search}%'`
-    );
-
-    res
-      .status(200)
-      .json({ persons: person_result.rows, companies: company_result.rows });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
-
-// Get person transactions
-app.get('/api/search/person/:personid', async (req, res) => {
-  try {
-    const { personid } = req.params;
-
-    const personid_trans = await db.query(
-      `SELECT * FROM transaction WHERE person_uid = '${personid}'`
-    );
-
-    const person_name = await db.query(
-      `SELECT name from person WHERE person_uid = '${personid}'`
-    );
-
-    res
-      .status(200)
-      .json({ person: person_name.rows[0], trans: personid_trans.rows });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
-
-// Get company transactions
-app.get('/api/search/company/:ticker', async (req, res) => {
-  try {
-    const { ticker } = req.params;
-
-    const ticker_trans = await db.query(
-      `SELECT * FROM transaction WHERE ticker = '${ticker}'`
-    );
-
-    const ticker_name = await db.query(
-      `SELECT company as name FROM company WHERE ticker = '${ticker}'`
-    );
-
-    res
-      .status(200)
-      .json({ company: ticker_name.rows[0], trans: ticker_trans.rows });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
+app.use('/api/search', search);
 
 app.post('/api/member/', async (req, res) => {
   const { first_name, last_name, email, password } = req.body;
@@ -399,6 +311,6 @@ app.delete('/api/member/person', auth, async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log('server has started on port 5000');
 });
